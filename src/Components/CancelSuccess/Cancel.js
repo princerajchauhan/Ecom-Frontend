54a9afbac7ed7fade122e14df0b093441bb7f1a2
Prince <princeraj.chauhan@outlook.com>
2023-12-15 10:54:52 +0530

    success cancel

--- src/Components/CancelSuccess/Cancel.js
+++ src/Components/CancelSuccess/Cancel.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaHome } from "react-icons/fa";
 
 const Cancel = () =>{
     return(
@@ -6,6 +8,19 @@ const Cancel = () =>{
             <h2>Your Order has been Canceled Successfully !!</h2>
             
             {/* <h5>Your Order Number is: <span style={{fontWeight: '400'}}>{Math.floor(Math.random()*10000000000000)}</span></h5> */}
+            <h4>
+                <Link to="/"
+                    style={{
+                        textDecoration: 'none',
+                        color: 'red',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap:'5px'
+                    }}>
+                   <FaHome style={{color:'green'}} /> Home
+                </Link>
+            </h4>
         </div>
     )
 }
--- src/Components/CancelSuccess/Success.js
+++ src/Components/CancelSuccess/Success.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaHome } from "react-icons/fa";
 
-const Success = () =>{
-    return(
-        <div style={{margin: '30px auto', textAlign:'center'}}>
+const Success = () => {
+    return (
+        <div style={{ margin: '30px auto', textAlign: 'center' }}>
             <h2>Thank You for Your order !!</h2>
-            
-            <h5>Your Order Number is: <span style={{fontWeight: '400'}}>{Math.floor(Math.random()*10000000000000)}</span></h5>
+
+            <h5>Your Order Number is: <span style={{ fontWeight: '400' }}>{Math.floor(Math.random() * 10000000000000)}</span></h5>
+            <h4>
+                <Link to="/"
+                    style={{
+                        textDecoration: 'none',
+                        color: 'red',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap:'5px'
+                    }}>
+                   <FaHome style={{color:'green'}} /> Home
+                </Link>
+            </h4>
         </div>
     )
 }
